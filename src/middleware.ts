f58b7fe430f0b2748beaf1cf349a16b193947a65
seyaendris <seide9894@gmail.com>
2024-09-19 20:49:29 +0300

    middleware created

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { authMiddleware } from '@clerk/nextjs'
+
+export default authMiddleware({
+    publicRoutes: [
+        '/',
+        '/auth(.*)',
+        '/portal'
+    ],
+    ignoredRoutes: [
+        'chatbot'
+    ]
+})
+
+export const config = {
+    matcher: ['/((?!.+.[w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  }
